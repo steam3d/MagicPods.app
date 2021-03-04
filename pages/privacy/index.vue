365362bba4ed0fb5d49d7cda398d1ada97bb4b8e
vladky_94 <kvladky@gmail.com>
2021-03-04 23:36:53 +0300

    Add privacy page

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-container
-    fluid
-    class="d-flex align-center justify-center fill-height"
-    style="max-width: 80%"
-  >
+  <v-container fluid class="d-flex align-center justify-center fill-height">
+    <div
+      class="d-flex flex-wrap justify-center align-center flex-column"
+      style="max-width: 628px"
+    >
+      <h5 class="text-h5 font-weight-bold pb-2">Privacy policy</h5>
+      <h3 class="text-h3 font-weight-bold pb-12 text-center">
+        MagicPods doesn't collect any user data
+      </h3>
+    </div>
   </v-container>
 </template>
 
@@ -13,5 +18,3 @@ import { Vue, Component } from 'vue-property-decorator'
 @Component({})
 export default class Index extends Vue {}
 </script>
-
-<style lang="scss"></style>
