f8b291dac454279d5a9244a907f07300fbb225da
Alexander Maslov <steam3d@gmail.com>
2023-04-06 15:46:10 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'MagicPods',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Add a little magic to your AirPods✨'
+        }
+      ],      
       link: [
         {
           rel: 'preconnect',
